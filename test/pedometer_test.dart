39259d5630164f318295062c42d6d5ca005b33a8
LootExe <114713024+LootExe@users.noreply.github.com>
2022-10-16 20:09:46 +1100

    updated test class to include getStepCount function

--- test/pedometer_test.dart
+++ test/pedometer_test.dart
@@ -7,9 +7,11 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockPedometerPlatform
     with MockPlatformInterfaceMixin
     implements PedometerPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<int?> getStepCount() => Future.value(1234);
 }
 
 void main() {
@@ -25,5 +27,13 @@ void main() {
     PedometerPlatform.instance = fakePlatform;
 
     expect(await pedometerPlugin.getPlatformVersion(), '42');
+  });
+
+  test('getStepCount', () async {
+    Pedometer pedometerPlugin = Pedometer();
+    MockPedometerPlatform fakePlatform = MockPedometerPlatform();
+    PedometerPlatform.instance = fakePlatform;
+
+    expect(await pedometerPlugin.getStepCount(), 1234);
   });
 }
