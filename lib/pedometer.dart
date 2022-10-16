ba80610bd60fbc7b2caca67069d9cb64d02afaad
LootExe <114713024+LootExe@users.noreply.github.com>
2022-10-16 21:26:42 +1100

    removed method channel from dart file

--- lib/pedometer.dart
+++ lib/pedometer.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/services.dart';
 
 class Pedometer {
-  static const _methodChannel = MethodChannel('com.lootexe.pedometer');
   static const _eventChannel = EventChannel('com.lootexe.pedometer.event');
-
-  static Future<int> getLastStepCount() async {
-    try {
-      final result = await _methodChannel.invokeMethod<int?>('getStepCount');
-      return result ?? -1;
-    } on PlatformException {
-      return -1;
-    }
-  }
 
   static Stream<int> getStepCountStream() =>
       _eventChannel.receiveBroadcastStream().map((steps) => steps);
