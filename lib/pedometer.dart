0c1ad3fff70125e52495ebe27abfd8caaf55e870
LootExe <114713024+LootExe@users.noreply.github.com>
2022-10-23 22:50:22 +1100

    added configuration class. eventChannel passes config as arguments to the plugin. returned data is transformed from json into int

--- lib/pedometer.dart
+++ lib/pedometer.dart
@@ -1,8 +1,30 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/services.dart';
 
 class Pedometer {
-  static const _eventChannel = EventChannel('com.lootexe.pedometer.event');
+  static const _eventChannel =
+      EventChannel('com.lootexe.pedometer.sensor', JSONMethodCodec());
 
-  static Stream<int> getStepCountStream() =>
-      _eventChannel.receiveBroadcastStream().map((steps) => steps);
+  static Stream<int> getStepCountStream(SensorConfiguration configuration) {
+    return _eventChannel
+        .receiveBroadcastStream(configuration.toJson())
+        .map((event) => event['stepCount']);
+  }
+}
+
+class SensorConfiguration {
+  const SensorConfiguration({
+    required this.batchingInterval,
+  });
+
+  final Duration batchingInterval;
+
+  Map toJson() {
+    Map json = {
+      'batching': max(batchingInterval.inMilliseconds, 100),
+    };
+    return json;
+  }
 }
