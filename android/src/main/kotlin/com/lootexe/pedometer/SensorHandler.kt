c5696872589a13954f5caf8c623825942308f1bf
LootExe <114713024+LootExe@users.noreply.github.com>
2022-10-23 22:48:42 +1100

    rewrote sensorListener. eventStream now gets configuration as passed as arguments in a json format. sensorManager.registerListener can now be configured with batchingInterval

--- android/src/main/kotlin/com/lootexe/pedometer/SensorHandler.kt
+++ android/src/main/kotlin/com/lootexe/pedometer/SensorHandler.kt
@@ -5,43 +5,81 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import io.flutter.plugin.common.EventChannel
 
-class SensorHandler(context: Context) : EventChannel.StreamHandler, SensorEventListener {
-    private val sensorManager: SensorManager
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.JSONMethodCodec
+import org.json.JSONException
+import org.json.JSONObject
+
+class SensorHandler(context: Context,
+                    messenger: BinaryMessenger): EventChannel.StreamHandler {
+    private val eventChannel: EventChannel
+    private val sensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     private var sensor: Sensor? = null
-    private var eventSink: EventChannel.EventSink? = null
+    private var sensorListener: SensorEventListener? = null
 
     init {
-        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        eventChannel = EventChannel(messenger,
+            "com.lootexe.pedometer.sensor", JSONMethodCodec.INSTANCE)
+        eventChannel.setStreamHandler(this)
+
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        if (p0 == null || eventSink == null) {
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
+        if (arguments == null) {
+            events.error("-1","No arguments specified", "")
             return
         }
 
-        eventSink?.success(p0.values[0].toInt())
-    }
+        var config: SensorConfiguration? = null
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+        try {
+           config = SensorConfiguration.fromJson(arguments as JSONObject)
+        }
+        catch (e: JSONException) {
+            events.error("-1","error", "JSON error: " + e.message)
+        }
 
-    override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
+        if (config == null) {
+            return
+        }
+
         if (sensor == null) {
-            events.error("-1",
-                "Sensor not available",
+            events.error("-1","Sensor not available",
                 "No pedometer hardware found on this device")
             return
         }
 
-        eventSink = events
+        sensorListener = object : SensorEventListener {
+            override fun onSensorChanged(p0: SensorEvent?) {
+                if (p0 == null) {
+                    return
+                }
+
+                val json = JSONObject().apply {
+                    put("stepCount", p0.values[0].toInt())
+                }
+
+                events.success(json)
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+        }
+
         sensorManager.registerListener(
-            this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            sensorListener,
+            sensor,
+            SensorManager.SENSOR_DELAY_NORMAL,
+            config.batchingInterval)
     }
 
     override fun onCancel(arguments: Any?) {
-        sensorManager.unregisterListener(this)
-        eventSink = null
+        sensorManager.unregisterListener(sensorListener)
+    }
+
+    fun dispose() {
+        eventChannel.setStreamHandler(null)
     }
 }
